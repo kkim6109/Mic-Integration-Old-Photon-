f922e2d33a1760afeceee4eecc1947e44904cda6
kkim6109 <46933251+kkim6109@users.noreply.github.com>
2019-11-14 23:50:57 -0600

    Add files via upload

---
+++ MicEF.cs
@@ -0,0 +1,134 @@
+﻿// Created By: Elite Future, Discord: Elite Future#1043 for questions, suggestions, or optimizations
+
+using UnityEngine;
+using System;
+using System.Threading;
+using System.Collections.Generic;
+
+public class MicEF : MonoBehaviour
+{
+
+    private int lastPos;
+    private AudioClip c;
+    public static float FREQUENCY = 10000f;
+    public static int DECREASE = 125;
+    private int threadID;
+    public static Dictionary<int, MicPlayer> users;
+    public static KeyCode pushToTalk = KeyCode.Z;
+
+    void Start()
+    {
+        users = new Dictionary<int, MicPlayer>(); // int for ID
+        DECREASE = (int)(FREQUENCY / 80f); // This is to slightly slow down the audio, not noticeable sound-wise, but removes some gaps
+        threadID = -1;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(pushToTalk))
+        {
+            sendMicData(); // sends remaining mic data
+            threadID = -1;
+            lastPos = 0;
+            Microphone.End(null);
+        }
+        else if (Input.GetKey(pushToTalk) && threadID == -1)
+        {
+            // Too lazy to actually put this onto onjoin, so ez pz send that you have mic to everyone every time you use your mic
+            RaiseEventOptions raised = new RaiseEventOptions();
+            raised.Receivers = ReceiverGroup.Others;
+            PhotonNetwork.networkingPeer.OpRaiseEvent((byte)173, new float[0], false, raised);
+
+            c = Microphone.Start(null, true, 100, (int)FREQUENCY);
+            
+            threadID = UnityEngine.Random.Range(0, Int32.MaxValue);
+            new Thread(() =>
+            {
+                try
+                {
+                    int ID = threadID;
+                    Thread.CurrentThread.IsBackground = true;
+                    while (ID == threadID) // Just in case 2 instances of the thread is up, it'll stop the old one
+                    {
+                        sendMicData();
+                        // Delay to make larger packets and less gaps(so less choppiness)
+                        Thread.Sleep(300); // You can adjust it as you like to see what the best combo is for low latency + low choppiness
+                    }
+                }
+                catch (Exception e)
+                {
+                    RPCList.add(e.Message); // Debugger
+                }
+            }).Start();
+        }
+    }
+
+    // Removes player so that it doesn't send stuff unnecessarily
+    public void OnPhotonPlayerDisconnected(PhotonPlayer player)
+    {
+        if (users.ContainsKey(player.ID))
+        {
+            users.Remove(player.ID);
+        }
+    }
+
+    // Sends Mic data
+    private void sendMicData()
+    {
+        if (users.Count > 0)
+        {
+            int pos = Microphone.GetPosition(null);
+            if (pos < lastPos) // If the microphone loops, the last sample needs to loop too
+            {
+                lastPos = 0;
+            }
+            int diff = pos - lastPos;
+            if (diff > 4000) // Just so you don't send something too big, shouldn't happen unless you adjust the frequency too high
+            {
+                RPCList.add("Packet Too Big"); // Debugger
+            }
+            else if (diff > 0) // So it doesn't send an empty float[]
+            {
+                float[] samples = new float[diff];
+                c.GetData(samples, lastPos);
+                RaiseEventOptions raised = new RaiseEventOptions();
+                int[] targets = new int[users.Count];
+                users.Keys.CopyTo(targets, 0);
+                PhotonNetwork.networkingPeer.OpRaiseEvent((byte)173, samples, false, raised);
+            }
+            lastPos = pos;
+        }
+    }
+}
+
+/*
+ * Add to OnEvent in networking peer between some other events to make it simple to add
+case 173:
+try // In case there isn't a float[], quick and ez for lazy people to do
+{
+    float[] f = (float[])photonEvent[0xf5];
+
+    // Identifier so they can add them to the list on join
+    if (!MicEF.users.ContainsKey(sender.ID))
+    {// I know that this will make the person who joined send 0 twice(one on entry one in return) but that doesn't really matter
+        MicEF.users.Add(sender.ID, new MicPlayer(sender.ID));
+        RaiseEventOptions raised = new RaiseEventOptions();
+        raised.TargetActors = new int[] { sender.ID };
+        PhotonNetwork.networkingPeer.OpRaiseEvent((byte)173, new float[0], false, raised);
+    }
+    else if (f.Length > 0)
+    {
+        AudioClip clip = AudioClip.Create(UnityEngine.Random.Range(float.MinValue, float.MaxValue) + "", f.Length, 1, (int)MicEF.FREQUENCY - MicEF.DECREASE, true, false);
+        clip.SetData(f, 0);
+        MicEF.users[sender.ID].add(clip);
+    }
+}
+catch (Exception e)
+{
+    RPCList.add(e.Message); // Debugger
+}
+return;
+*/
+
+// base.gameObject.AddComponent<MicEF>(); // in Fenggame start or wherever you want
+// Also don't forget to add an exception for event 173 from your antis!!!
---
+++ MicPlayer.cs
@@ -0,0 +1,95 @@
+﻿// Created By: Elite Future, Discord: Elite Future#1043 for questions, suggestions, or optimizations
+
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class MicPlayer
+{
+    private float micModifier = 1f; // Used to adjust audio wait times(Look in the IEnum)
+    private bool clipProcess = false; // To know if the IEnum is running
+    private Queue<AudioClip> clipQueue = new Queue<AudioClip>(); // Queue of clips itself
+    private int playerID = -1;
+    private float micVolume = 1.5f;
+
+    // Maybe an Icon as well if wanted
+
+    // Sets up the mic player, not much else
+    public MicPlayer(int pID)
+    {
+        if (CacheGameObject.Find("MainCamera").GetComponent<AudioSource>() == null)
+        {
+            CacheGameObject.Find("MainCamera").AddComponent<AudioSource>();
+        }
+        playerID = pID;
+    }
+
+    // Potential use for future identification
+    public int ID
+    {
+        get
+        {
+            return playerID;
+        }
+    }
+
+    // Sets and gets the volume of the specific person [Make a GUI later]
+    public float volume
+    {
+        get
+        {
+            return micVolume;
+        }
+        set
+        {
+            micVolume = value;
+        }
+    }
+
+    // Adds an audioclip to the queue
+    public void add(AudioClip clip)
+    {
+        clipQueue.Enqueue(clip);
+        if (!clipProcess)
+        {
+            FengGameManagerMKII.fengGame.StartCoroutine(waitTimeTillNext());
+        }
+    }
+
+    // Processes and plays the queue of clips for a smooth voice effect
+    public IEnumerator waitTimeTillNext()
+    {
+        if (!clipProcess)
+        {
+            clipProcess = true;
+        }
+        if (clipQueue.Count > 0)
+        {
+            // Debug to see how the long the queue is
+            RPCList.add(clipQueue.Count + "");
+
+            AudioClip clip = clipQueue.Dequeue();
+            CacheGameObject.Find("MainCamera").GetComponent<AudioSource>().PlayOneShot(clip, micVolume);
+
+            // This makes it so that the queue doesn't get too long, the stiched audios also sounds a bit better at 0.98, but otherwise unnoticeable
+            if (micModifier == 1f && clipQueue.Count >= 4)
+            {
+                micModifier = 0.98f;
+            }
+            else if (micModifier == 0.98f && clipQueue.Count <= 2)
+            {
+                micModifier = 1f;
+            }
+
+            // Waits for the audio to be finished
+            yield return new WaitForSeconds(clip.length * micModifier);
+
+            // Repeats the IEnum for the potential next audio clip
+            FengGameManagerMKII.fengGame.StartCoroutine(waitTimeTillNext());
+        }
+        else
+        {
+            clipProcess = false;
+        }
+    }
+}
